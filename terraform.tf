2cd3574e6a72fdc7fcd22d04613bc74d28b864b1
Nic Jackson <jackson.nic@gmail.com>
2018-01-08 18:54:01 +0000

    Added example resource

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_s3_bucket" "b" {
+  bucket = "nic-sentinel-my-tf-test-bucket"
+  acl    = "private"
+
+  tags {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
