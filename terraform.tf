24f3230020f90ee403fdd252c62cb7f13827ebdc
Nic Jackson <jackson.nic@gmail.com>
2018-01-08 19:19:00 +0000

    Updated config

--- terraform.tf
+++ terraform.tf
@@ -2,8 +2,25 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "b" {
+provider "aws" {
+  alias  = "west"
+  region = "us-west-1"
+}
+
+resource "aws_s3_bucket" "a" {
   bucket = "nic-sentinel-my-tf-test-bucket"
+  acl    = "private"
+
+  tags {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "b" {
+  provider = "aws.west"
+
+  bucket = "nic-sentinel-my-tf-test-bucket2"
   acl    = "private"
 
   tags {
